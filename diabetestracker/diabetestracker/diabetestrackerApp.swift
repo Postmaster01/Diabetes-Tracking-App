ff659ab6b2acbdf1a057cf00a4ffb932e6c830ba
eduardoreyes007351208 <lalosoccer307@gmail.com>
2023-03-13 15:25:19 -0700

    adding the black app files

---
+++ diabetestracker/diabetestracker/diabetestrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  diabetestrackerApp.swift
+//  diabetestracker
+//
+//  Created by Eduardo Reyes on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct diabetestrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
